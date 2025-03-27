a6a27b618fe15de13aee6c2cf643f73ead724bb8
AleksZieba <zeeb3d@gmail.com>
2025-03-27 20:58:13 +0100

    reporting functionality added

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { argv } = require("process");
 const { crawlPage } = require("./crawl.js");
+const { printReport } = require("./report.js");
+
 
 async function main() {
     if (argv.length < 3) {
@@ -8,4 +10,14 @@ async function main() {
     if (argv.length > 3) {
       return console.log("Please provide only one URL to crawl");
     }
-} 
+
+    const baseUrl = argv[2];
+
+    console.log(`Crawling ${baseUrl}...`);
+  
+    pages = await crawlPage(baseUrl, baseUrl, {});
+  
+    printReport(pages);
+}  
+
+main();
